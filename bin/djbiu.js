47f17e2caf5b7bda5eec015b060f7a2d463f5b25
丁锐 <dingrui@daojia.com>
2018-04-19 22:49:52 +0800

    删除从命令行传入启动端口的功能

--- bin/djbiu.js
+++ bin/djbiu.js
@@ -5,15 +5,15 @@ const packageInfo = require('../package.json')
 
 program
     .version(packageInfo.version)
-    .usage("\n\t1. -n '项目名称' -p '启动服务的端口号'\n\t2. -t '表名' -m '类名'")
+    .usage("\n\t1. djbiu init -n '项目名称' -p '启动服务的端口号'\n\t2. djbiu crud -t '表名' -m '类名'")
 
 program
     .option('-n, --name [value]', '项目名称')
     .option('-p, --port <n>', '启动服务的端口号，默认是3000')
     .command('init', '创建新项目')
-    .action(() => {
-        let projectName = program.name
-        let port = program.port
+    .action((cmd, options) => {
+        let projectName = options.name
+        let port = options.port
 
         if (projectName && port) {
             const metadata = {
@@ -29,9 +29,9 @@ program
     .option('-t, --table [value]', '对应的表名')
     .option('-m, --model [value]', '对应的类名')
     .command('crud', '自动生成model的增删改查')
-    .action(() => {
-        modelName = program.model
-        tableName = program.table
+    .action((cmd, options) => {
+        modelName = options.model
+        tableName = options.table
 
         if (modelName && tableName) {
             const metadata = {
